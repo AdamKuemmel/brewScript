69767de43e2af268d52f8a6594b4d67b5fa084b7
bcbridges <bcb35b@gmail.com>
2021-10-05 08:21:56 -0600

    Added product model to server side.

---
+++ server/models/Product.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const productSchema = new Schema({
+  product_name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  item_cost: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+  themes: [
+    {
+      theme_name: {
+        type: String,
+        required: false,
+        trim: true,
+      },
+    },
+  ],
+});
+
+const Product = model("Product", productSchema);
+
+module.exports = Product;
