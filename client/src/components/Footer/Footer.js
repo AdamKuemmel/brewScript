7f578e25048ba3a15246b0cc3fe1b161848edfa9
AdamKuemmel <Adamkuemmel@gmail.com>
2021-10-06 23:28:55 -0600

    drop ups in footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 //import things needed in this file
 import React from "react";
 import { useLocation, useHistory } from "react-router-dom";
+import { Dropdown, DropdownButton } from "react-bootstrap";
 
 //style keys
 const styles = {};
@@ -11,6 +12,46 @@ const Footer = () => {
   const history = useHistory();
   return (
     <footer className="w-100 mt-auto text-dark p-4">
+      <Dropdown>
+        <Dropdown.Toggle
+          id="dropdown-button-dark-example1"
+          drop="up"
+          variant="secondary"
+        >
+          Contact
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu variant="dark">
+          <Dropdown.Item href="#/action-1" active>
+            About us
+          </Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Twitter</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Facebook</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item href="#/action-4">Instagram</Dropdown.Item>
+          <Dropdown.Item href="#/action-5">Youtube</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <Dropdown>
+        <Dropdown.Toggle
+          id="dropdown-button-dark-example1"
+          drop="up"
+          variant="secondary"
+        >
+          Company
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu variant="dark">
+          <Dropdown.Item href="#/action-1" active>
+            Our Story
+          </Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Team</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Careers</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item href="#/action-4">Blog</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
       <div className="container text-center mb-5">
         {location.pathname !== "/" && (
           <button
