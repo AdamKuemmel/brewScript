a9ad65c6ad795eb7afceb17448813eb7277cb2c4
AdamKuemmel <Adamkuemmel@gmail.com>
2021-10-06 23:07:22 -0600

    front end things, a lot of stuff is commented for error purpose

--- client/src/components/UserLand/AddOn.js
+++ client/src/components/UserLand/AddOn.js
@@ -1,38 +1,38 @@
-import React from "react";
-import { useLocation, useHistory } from "react-router-dom";
+// import React from "react";
+// import { useLocation, useHistory } from "react-router-dom";
 
-const AddonsList = ({ addons, title }) => {
-  if (!profiles.length) {
-    return <h3>No Products available!</h3>;
-  }
+// const AddonsList = ({ addons, title }) => {
+//   if (!profiles.length) {
+//     return <h3>No Products available!</h3>;
+//   }
 
-  return (
-    <div>
-      <h3 className="text-primary">{title}</h3>
-      <div className="flex-row justify-space-between my-4">
-        {addons &&
-          addons.map((addon) => (
-            <div key={addon._id} className="col-12 col-xl-6">
-              <div className="card mb-3">
-                <h4 className="card-header bg-dark text-light p-2 m-0">
-                  {addon.name} <br />
-                  <span className="text-white" style={{ fontSize: "1rem" }}>
-                    {addon.description}
-                  </span>
-                </h4>
+//   return (
+//     <div>
+//       <h3 className="text-primary">{title}</h3>
+//       <div className="flex-row justify-space-between my-4">
+//         {addons &&
+//           addons.map((addon) => (
+//             <div key={addon._id} className="col-12 col-xl-6">
+//               <div className="card mb-3">
+//                 <h4 className="card-header bg-dark text-light p-2 m-0">
+//                   {addon.name} <br />
+//                   <span className="text-white" style={{ fontSize: "1rem" }}>
+//                     {addon.description}
+//                   </span>
+//                 </h4>
 
-                <Link
-                  className="btn btn-block btn-squared btn-light text-dark"
-                  to={`/addons/${addons._id}`}
-                >
-                  Go to item
-                </Link>
-              </div>
-            </div>
-          ))}
-      </div>
-    </div>
-  );
-};
+//                 <Link
+//                   className="btn btn-block btn-squared btn-light text-dark"
+//                   to={`/addons/${addons._id}`}
+//                 >
+//                   Go to item
+//                 </Link>
+//               </div>
+//             </div>
+//           ))}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default AddOn;
+// export default AddOn;
