5f692fa1ddef3c70785bbcd94ae54a53f63b74e3
friel2021 <friel2009@gmail.com>
2021-10-10 21:33:06 -0600

    updated addoncenter

--- client/src/components/UserLand/AddOn.js
+++ client/src/components/UserLand/AddOn.js
@@ -78,7 +78,7 @@ const AddOn = () => {
   return (
     <div>
       <h1 style={styles.titles}>Anything to add to your next order?</h1>
-      <div className="row row-cols-1 row-cols-md-2 ">
+      <div className="row row-cols-1 justify-content-center row-cols-md-2 ">
         {loading ? (
           <h1>Loading</h1>
         ) : (
